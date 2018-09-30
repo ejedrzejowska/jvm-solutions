ffb0217be0eaafe7f1df2adb9403aed9a8a2fbe5
Ewa Jedrzejowska <ewa.jedrzejowska@gmail.com>
2018-09-30 11:44:22 +0200

    First solution

--- 01_javac/HelloWorld.java
+++ 01_javac/HelloWorld.java
@@ -1,4 +1,6 @@
 public class HelloWorld {
+    private class HelloPoland{
+    }
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }
---
+++ 01_javac/world/pl/lodz/srodmiescie/HelloPiotrkowska.java
@@ -0,0 +1,7 @@
+package world.pl.lodz.srodmiescie;
+
+public class HelloPiotrkowska {
+    public static void main(String[] args) {
+        System.out.println("I'm so looong");
+    }
+}
